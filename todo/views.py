9a7e40edc701a79c672f25c2fe49543c5c18edad
Krzysztof Walczak <christopher.walczak@gmail.com>
2023-05-07 17:44:13 +0100

    Add task functionality

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Task
+
+
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
 
 # Create your views here.
--- todo_main/urls.py
+++ todo_main/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('todo/', include('todo.urls'))
 ]
