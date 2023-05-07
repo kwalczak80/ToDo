bbf0805377db151c94efcacb46727ce6f4a6855c
Krzysztof Walczak <christopher.walczak@gmail.com>
2023-05-07 20:22:28 +0100

    Add edit task functionality

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
+from django.shortcuts import get_object_or_404
 
 
 def addTask(request):
@@ -8,4 +9,29 @@ def addTask(request):
     Task.objects.create(task=task)
     return redirect('home')
 
-# Create your views here.
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        get_task.task = request.POST['task']
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html', context)
